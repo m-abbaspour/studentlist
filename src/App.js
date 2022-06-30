c069d9f113073ce14744b8ff6d6e8e48bf278060
Mahdiyeh Abbaspour <abbspourmahdiyeh@gmail.com>
2022-06-30 09:50:16 -0600

    * Imports bootstarp to project * Imports the Card API * Adds Student Card to Projects

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
 import Home from './Component/Home';
 import About from './Component/About';
 import Contact from './Component/Contact';
+import StudentCard from './Component/StudentCard';
 import './App.css';
 
 class App extends Component {
@@ -11,20 +12,24 @@ render() {
 	<Router>
 		<div className="App">
 			<ul className="App-header">
-			<li>
-				<Link to="/">Home</Link>
-			</li>
-			<li>
-				<Link to="/about">About Us</Link>
-			</li>
-			<li>
-				<Link to="/contact">Contact Us</Link>
-			</li>
+				<li>
+					<Link to="/">Home</Link>
+				</li>
+				<li>
+					<Link to="/about">About Us</Link>
+				</li>
+				<li>
+					<Link to="/contact">Contact Us</Link>
+				</li>
+				<li>
+					<Link to="/studentlist">Student List</Link>
+				</li>
 			</ul>
 		<Routes>
 				<Route exact path='/' element={< Home />}></Route>
 				<Route exact path='/about' element={< About />}></Route>
 				<Route exact path='/contact' element={< Contact />}></Route>
+				<Route exact path='/studentlist' element={< StudentCard />}></Route>
 		</Routes>
 		</div>
 	</Router>
---
+++ src/component/StudentCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+// import './Style.css'
+import Card from 'react-bootstrap/Card'
+// to be able to use bootstrap run this command:
+// npm install react-bootstrap bootstrap
+
+function StudentCard() {
+  return (
+    <div>    
+        <Card>
+        
+        </Card> 
+    </div>
+  )
+}
+
+export default StudentCard
