89743dfad632d4b053a4a22bf0954b40a6474363
Mahdiyeh Abbaspour <abbspourmahdiyeh@gmail.com>
2022-06-30 13:12:03 -0600

    Updates the StudentCard.js and Styles.css * Edits the <Card> * adds Images to the Cards * Updates the Styling of the Card

--- src/component/StudentCard.js
+++ src/component/StudentCard.js
@@ -1,15 +1,32 @@
 import React from 'react'
-// import './Style.css'
+import './Style.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
 // to be able to use bootstrap run this command:
 // npm install react-bootstrap bootstrap
 
 function StudentCard() {
   return (
-    <div>    
-        <Card>
-        
-        </Card> 
+    <div className='size'>    
+    <Row xs={1} md={2} className="g-4">
+      {Array.from({ length: 4 }).map((_, idx) => (
+        <Col>
+          <Card>
+            <Card.Img variant="top" src="./s1.png" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit longer.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
     </div>
   )
 }
