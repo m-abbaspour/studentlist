d6f6aec2038783edac802115fee7f4faea04cd68
Mahdiyeh Abbaspour <abbspourmahdiyeh@gmail.com>
2022-06-30 15:46:54 -0600

    Finishing touches to project * Replaces StudentCard with Student list in App.js * Adds stylying to Navbar * Adds StudentList.js Containing the Students * Edits the Card with new info

--- src/App.js
+++ src/App.js
@@ -3,33 +3,35 @@ import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
 import Home from './Component/Home';
 import About from './Component/About';
 import Contact from './Component/Contact';
-import StudentCard from './Component/StudentCard';
 import './App.css';
-
+import './Component/Style.css'
+import StudentList from './Component/StudentList';
 class App extends Component {
 render() {
 	return (
 	<Router>
-		<div className="App">
-			<ul className="App-header">
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li>
-					<Link to="/about">About Us</Link>
-				</li>
-				<li>
-					<Link to="/contact">Contact Us</Link>
-				</li>
-				<li>
-					<Link to="/studentlist">Student List</Link>
-				</li>
-			</ul>
+		<div>
+			<div className="myNavbar">
+				<ul className='size'>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/about">About Us</Link>
+					</li>
+					<li>
+						<Link to="/contact">Contact Us</Link>
+					</li>
+					<li>
+						<Link to="/studentlist">Student List</Link>
+					</li>
+				</ul>
+			</div>
 		<Routes>
 				<Route exact path='/' element={< Home />}></Route>
 				<Route exact path='/about' element={< About />}></Route>
 				<Route exact path='/contact' element={< Contact />}></Route>
-				<Route exact path='/studentlist' element={< StudentCard />}></Route>
+				<Route exact path='/studentlist' element={< StudentList />}></Route>
 		</Routes>
 		</div>
 	</Router>
---
+++ src/component/StudentList.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import StudentCard from './StudentCard'
+import s1 from './s1.png'
+import s2 from './s2.png'
+import s3 from './s3.png'
+import s4 from './s4.png'
+
+function StudentList() {
+    const students = [
+        {
+            id:1,
+        image: s1,
+        name: "Mike",
+        gpa: 3.8
+      },
+      {
+        id:2,
+        image: s2,
+          name: "Manal",
+          gpa: 3.6
+      },
+      {
+        id:3,
+        image: s3,
+          name: "Moe",
+          gpa: 3.2
+      },
+      {
+        id:4,
+        image: s4,
+          name: "Mary",
+          gpa: 2.6
+      }
+    ]
+
+    const studentList = students.map(a => <StudentCard key={a.id} student = {a}/>
+        )
+  return (
+    <div className='cardList'>{studentList}</div>
+  )
+}
+
+export default StudentList
