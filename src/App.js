edadfe2fd0ca6ac928e354ad400ae3003ecfa2d7
Mahdiyeh Abbaspour <abbspourmahdiyeh@gmail.com>
2022-06-30 08:28:56 -0600

    Chenges App.js and App.css * Deletes App function in App.js * Adds imports and Routes to App.js *Changes Header size in App.css

--- src/App.js
+++ src/App.js
@@ -1,56 +1,35 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
+import Home from './Component/Home';
+import About from './Component/About';
+import Contact from './Component/Contact';
 import './App.css';
-import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-	Link
-} from 'react-router-dom';
 
 class App extends Component {
-  render() {
-    return (
-    <Router>
-      <div className="App">
-      </div>
-    </Router>
-  );
-  }
-  }
-  
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div className="App">
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/about">About Us</Link>
-      </li>
-      <li>
-        <Link to="/contact">Contact Us</Link>
-      </li>
-    </ul>
-</div>
-
-    </div>
-  );
+render() {
+	return (
+	<Router>
+		<div className="App">
+			<ul className="App-header">
+			<li>
+				<Link to="/">Home</Link>
+			</li>
+			<li>
+				<Link to="/about">About Us</Link>
+			</li>
+			<li>
+				<Link to="/contact">Contact Us</Link>
+			</li>
+			</ul>
+		<Routes>
+				<Route exact path='/' element={< Home />}></Route>
+				<Route exact path='/about' element={< About />}></Route>
+				<Route exact path='/contact' element={< Contact />}></Route>
+		</Routes>
+		</div>
+	</Router>
+);
+}
 }
 
 export default App;
